bf48633495220cd0e40e5c3ea35474ff04d139c6
leonardo rocha <leonardo.rocha1990@gmail.com>
2021-10-08 20:45:40 -0500

    Simple updates

--- src/test/java/com/leonardo/rocha/gatewayDemo/controller/GatewayControllerTest.java
+++ src/test/java/com/leonardo/rocha/gatewayDemo/controller/GatewayControllerTest.java
@@ -1,4 +1,28 @@
+package com.leonardo.rocha.gatewayDemo.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
+
 import static org.junit.Assert.*;
+
+
 public class GatewayControllerTest {
-  
+    GatewayController uut;
+    @Before
+    public void setUp() throws Exception {
+        uut = new GatewayController();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getTest() {
+        ResponseEntity<String> response = uut.getTest();
+        assertEquals(response.getBody(),"Hello, World!");
+    }
 }
