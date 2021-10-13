cf0ccc98242654a0f58ebd30c46372c9cfd7fed4
leonardo rocha <leonardo.rocha1990@gmail.com>
2021-10-13 00:53:59 -0500

    Adding mysql test code

--- src/main/java/com/leonardo/rocha/gatewayDemo/GatewayDemoApplication.java
+++ src/main/java/com/leonardo/rocha/gatewayDemo/GatewayDemoApplication.java
@@ -1,5 +1,7 @@
 package com.leonardo.rocha.gatewayDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAutoConfiguration
 public class GatewayDemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(GatewayDemoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Updated code");
 		SpringApplication.run(GatewayDemoApplication.class, args);
 	}
 
